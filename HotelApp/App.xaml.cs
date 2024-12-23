4a0092618d1dd47f1d4f3c6d341de89125f22cd1
IvanHS <ivanhenriquessilva@gmail.com>
2024-12-23 13:07:28 -0300

    Set screen size

--- HotelApp/App.xaml.cs
+++ HotelApp/App.xaml.cs
@@ -1,15 +1,26 @@
-﻿namespace HotelApp
+﻿using Microsoft.Extensions.Logging;
+
+namespace HotelApp
 {
-    public partial class App : Application
-    {
+    public partial class App : Application {
+
+        const int WindowWidth = 400;
+        const int WindowHeight = 750;
+        const int WindowXPos = 100;
+        const int WindowYPos = 100;
+
+
         public App()
         {
             InitializeComponent();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        protected override Window CreateWindow(IActivationState? activationState) =>
+            new(new AppShell()) {
+                Width = WindowWidth,
+                Height = WindowHeight,
+                X = WindowXPos,
+                Y = WindowYPos
+            };
     }
 }
